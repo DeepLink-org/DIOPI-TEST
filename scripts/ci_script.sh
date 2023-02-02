fdcc2b742184132a7c86aca1509000d7f96a61d6
HU-qingqing <huqingqing@sensetime.com>
2023-02-02 17:58:21 +0800

    fix

--- scripts/ci_script.sh
+++ scripts/ci_script.sh
@@ -4,11 +4,12 @@ set -e
 case $1 in
   py-lint)
     (echo "py-lint" && flake8 --ignore=E501,F841 python/conformance/diopi_functions.py \
-       && flake8 --ignore=E501,F401 --exclude=python/conformance/diopi_functions.py) \
+       && flake8 --ignore=E501,F401 --exclude=python/conformance/diopi_functions.py,scripts/cpplint.py) \
     || exit -1;;
   cpp-lint)
-    (echo "cpp-lint" && python scripts/cpplint.py --linelength=160 --filter=-runtime/references,-legal/copyright \
-      --filter=-runtime/printf,-runtime/int,-whitespace/indent,-build/namespaces --recursive impl/ \
+    # for other cpplint version, maybe -runtime/printf -legal/copyright -whitespace/indent
+    # -build/namespaces -runtime/references is needed to check impl
+    (echo "cpp-lint" && python scripts/cpplint.py --linelength=160 --filter=-runtime/int --recursive impl/ \
       && python scripts/cpplint.py --linelength=240 --filter=-build/header_guard --recursive diopirt/ ) \
     || exit -1;;
     *)
