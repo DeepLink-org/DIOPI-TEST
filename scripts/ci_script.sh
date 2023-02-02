0ff15f329353d540fa386c74ca73459e339a35c3
HU-qingqing <huqingqing@sensetime.com>
2023-02-02 17:41:46 +0800

    fix

--- scripts/ci_script.sh
+++ scripts/ci_script.sh
@@ -7,9 +7,9 @@ case $1 in
        && flake8 --ignore=E501,F401 --exclude=python/conformance/diopi_functions.py) \
     || exit -1;;
   cpp-lint)
-    (echo "cpp-lint" && cpplint --linelength=160 --filter=-runtime/references,-legal/copyright \
+    (echo "cpp-lint" && python scripts/cpplint.py --linelength=160 --filter=-runtime/references,-legal/copyright \
       --filter=-runtime/printf,-runtime/int,-whitespace/indent,-build/namespaces --recursive impl/ \
-      && cpplint --linelength=240 --filter=-build/header_guard --recursive diopirt/ ) \
+      && python scripts/cpplint.py --linelength=240 --filter=-build/header_guard --recursive diopirt/ ) \
     || exit -1;;
     *)
     echo -e "[ERROR] Incorrect option:" $1;
