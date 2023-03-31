4e8326f66501b5ea3c2d6998bbc20e7d9a5fcce5
liujiaqi <liujiaqi@outlook.org.cn>
2023-03-31 12:50:53 +0800

    fix

--- python/conformance/diopi_functions.py
+++ python/conformance/diopi_functions.py
@@ -209,8 +209,10 @@ def abs(input, inplace=False) -> Tensor:
 def floor(input, inplace=False) -> Tensor:
     return unary_op(input, inplace, 'diopiFloor')
 
+
 def ceil(input, inplace=False) -> Tensor:
     return unary_op(input, inplace, 'diopiCeil')
+
 
 def sign(input) -> Tensor:
     return unary_op(input, False, 'diopiSign')
