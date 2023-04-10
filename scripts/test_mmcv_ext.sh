34b15d44101fd317873952bb524f0994e51d52e0
cokedong <408244909@qq.com>
2023-04-10 19:35:05 +0800

    Fix

--- scripts/test_mmcv_ext.sh
+++ scripts/test_mmcv_ext.sh
@@ -1,17 +1,25 @@
 # !/bin/bash
 set -e
 
+MMCV_TEST_LIST=(test_active_rotated_filter.py \
+test_assign_score_withk.py \
+test_bbox.py \
+test_deform_roi_pool.py \
+test_knn.py \
+test_convex_iou.py \
+test_min_area_polygons.py \
+test_prroi_pool.py \
+test_chamfer_distance.py \
+test_border_align.py
+)
+
 cd third_party/mmcv_diopi
 export PYTHONPATH=${PWD}:$PYTHONPATH
 cd tests/test_ops
-python -m pytest test_active_rotated_filter.py
-python -m pytest test_assign_score_withk.py
-python -m pytest test_bbox.py
-python -m pytest test_deform_roi_pool.py
-python -m pytest test_knn.py
-python -m pytest test_convex_iou.py
-python -m pytest test_min_area_polygons.py
-python -m pytest test_prroi_pool.py
-python -m pytest test_chamfer_distance.py
-python -m pytest test_border_align.py
+
+for elem in ${MMCV_TEST_LIST[@]}
+do
+    python -m pytest $elem
+done
+
 cd ../../../../
